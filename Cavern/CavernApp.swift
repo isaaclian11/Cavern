918bee708aaff27c0eedc2368faf46445ce0969c
Isaac Zahau <isaaclian11@gmail.com>
2022-01-29 10:42:16 -0600

    Initial Commit

---
+++ Cavern/CavernApp.swift
@@ -0,0 +1,17 @@
+//
+//  CavernApp.swift
+//  Cavern
+//
+//  Created by Isaac Zahau on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CavernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
